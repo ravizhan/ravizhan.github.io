3344b734b178a4fc09ed7bc1d594ec56c5d017e9
ravizhan <37010563+ravizhan@users.noreply.github.com>
2020-08-02 15:55:28 +0800

    Site updated: 2020-08-02 15:55:24

--- js/schemes/pisces.js
+++ js/schemes/pisces.js
@@ -84,3 +84,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
   Affix.init(document.querySelector('.sidebar-inner'), NexT.utils.getAffixParam());
 });
+
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+});
